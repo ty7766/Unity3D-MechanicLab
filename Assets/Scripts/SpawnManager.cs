97776afd628c175b8cf7ed2b3e8fd761cac68731
hyojoon Lim <hyojoon42@naver.com>
2025-06-17 20:02:19 +0900

    feature : Wave System 추가

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using System.Collections;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
+
+    public int enemyCount;
+    public int waveNumber = 1;
+
     private float spawnRange = 9;       //스폰 범위
 
     void Start()
     {
         //적 생성
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        SpawnEnemyWave(waveNumber);
+
+        //파워업 생성
+        SpawnPowerup();
     }
 
+    private void Update()
+    {
+        //FindObject 대신 태그를 이용하여 탐색
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        //Enemy가 맵에 없으면 웨이브 증가시키고 생성
+        if (enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            SpawnPowerup();
+        }
+    }
     //적 생성 알고리즘
     private Vector3 GenerateSpawnPosition()
     {
@@ -20,5 +42,20 @@ public class SpawnManager : MonoBehaviour
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
 
         return randomPos;
+    }
+
+    //여러 적 생성 메소드
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    //파워 업 생성 메소드
+    void SpawnPowerup()
+    {
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 }
